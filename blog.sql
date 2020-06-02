cbe52f2b85a991edabe30db145554910de893495
mursalinhawlader2258 <mursalinhawlader2258@gmail.com>
2020-06-03 02:49:51 +0600

    new server file create

--- blog.sql
+++ blog.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 07, 2020 at 01:00 PM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.1.12
+-- Generation Time: Jun 02, 2020 at 10:47 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -30,11 +29,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `blog_info` (
   `id` int(3) NOT NULL,
-  `category_name` text NOT NULL,
+  `category_id` int(5) NOT NULL,
   `blog_title` text NOT NULL,
   `short_description` text NOT NULL,
   `long_description` text NOT NULL,
-  `images` text NOT NULL,
+  `blog_image` text NOT NULL,
   `status` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -42,17 +41,9 @@ CREATE TABLE `blog_info` (
 -- Dumping data for table `blog_info`
 --
 
-INSERT INTO `blog_info` (`id`, `category_name`, `blog_title`, `short_description`, `long_description`, `images`, `status`) VALUES
-(3, '--Select Category Name--', 'ssssssssssssss', 'sssssssssssssssssssss', 'sssssssssssssssssssssssssssssss', '', ''),
-(4, '2', 'ddddddddddddddd', 'ddddddddddddd', 'ddddddddddddddddddddddddd', '', 'afa'),
-(5, '1', 'cccccccccccc', 'cccccccccccccccccc', 'cccccccccccccccccccccccccccccccc', '', ']'),
-(6, '1', 'fafafa123', '', 'afafaf123', '', '1]'),
-(7, '2', 'fafafa099999', '9afafafafa', 'afafa0987654', '', '1]'),
-(8, '--Select Category Name--', 'sumona', 'my second', 'love', '', ']'),
-(9, '--Select Category Name--', 'afaafa', 'afafafa', 'afafafa', '', ']'),
-(10, '--Select Category Name--', 'bbbbbbbbbbbbbbbbb', 'bbbbbbbbbbbbbbbbbbb', 'bbbbbbbbbbbbbbbbbbbbbbbbbbbb', '', ']'),
-(11, '2', 'ddddddddddddddd', 'ddddddddddddd', 'ddddddddddddddddddddddddd', '', 'afa]'),
-(12, '1', 'cccccccccccc', 'cccccccccccccccccc', 'cccccccccccccccccccccccccccccccc', '', ']]');
+INSERT INTO `blog_info` (`id`, `category_id`, `blog_title`, `short_description`, `long_description`, `blog_image`, `status`) VALUES
+(15, 0, 'musfiqur', 'afafadfafafa', 'afadsfasdfasfda', '../assets/images/pubg-img2.jpg', '1'),
+(16, 0, 'hjdhgdhafdafa', 'fafdafas', 'dfasdfadsfa', '../assets/images/1mg1.jpg', '1');
 
 -- --------------------------------------------------------
 
@@ -74,7 +65,8 @@ CREATE TABLE `category` (
 INSERT INTO `category` (`id`, `category_name`, `category_description`, `status`) VALUES
 (9, 'hokey', 'afafaafffffffffffffffffffffffffffffffffffffffgagagggggggggggggggggggggggggggfagagfaaaaaaaaaaaaaaa', '0'),
 (10, 'Football', 'Football is a family of team sports that involve, to varying degrees, kicking a ball to score a goal. Unqualified, the word football normally means the form of football that is the most popular where the word is used.', '0'),
-(11, 'Basketball', 'Basketball is a team sport in which two teams, most commonly of five players each, opposing one another on a rectangular court,', '0');
+(11, 'Basketball', 'Basketball is a team sport in which two teams, most commonly of five players each, opposing one another on a rectangular court,', '0'),
+(12, 'fdafaf', 'sadasdfasdfafda', '0');
 
 -- --------------------------------------------------------
 
@@ -126,13 +118,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `blog_info`
 --
 ALTER TABLE `blog_info`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `users`
